fe9a4d7b5a383b29c54184afc4086479340da1ba
BCYogesh <bcyogesh4961@gmail.com>
2025-04-20 01:03:16 +0530

    routing code push

--- src/app.js
+++ src/app.js
@@ -4,12 +4,26 @@ const app = express();
 
 app.listen(3000);
 
+// app.get("/hello", (req, res) => {
+//     res.send("Hello from hello")
+// });
+
+// app.post("/hello", (req, res) => {
+//     res.send("data saved successfully")
+// });
+
+app.get("/user/:userID/:name/:pass", (req, res) => {
+    console.log(req.params);
+    res.send({ firstName: "yogesh", lastName: "sekar" })
+});
+
+// all http methods request handle in this route
+app.all("/secret", (req, res) => {
+    res.send("secret routing")
+});
 
 
 
-app.get("/hello", (req, res) => {
-    res.send("Hello from thalapathy")
-})
 
 
 
