6e9df2e9a8b8a206ecfdce075abc88d005aaf7c1
BCYogesh <bcyogesh4961@gmail.com>
2025-04-21 12:42:20 +0530

    User Sign up api completed

--- src/app.js
+++ src/app.js
@@ -1,7 +1,29 @@
 const express = require('express');
 const connectDB = require('./config/database');
+const User = require('./models/user');
+
 
 const app = express();
+
+app.post('/signup', async (req, res) => {
+    const userData = new User({
+        firstName: "Ravindra",
+        lastName: "Jadeja",
+        mailId: "Jadeja@gmail.com",
+        password: "jadeja@123",
+        gender: "Male"
+    });
+
+    try {
+        // return promise
+        await userData.save();
+        res.send("User added successfully");
+    } catch (err) {
+        res.status(401).send("Something went wrong when signup to the user " + err.message)
+    }
+
+});
+
 
 connectDB().then(() => {
     console.log("DB connection established successfully");
--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,8 @@
-const moongose = require('mongoose');
+const mongoose = require('mongoose');
 
 
 const connectDB = async () => {
-    await moongose.connect("mongodb+srv://YogeshBC:Yogesh12345@yogesh.to9jano.mongodb.net/");
+    await mongoose.connect("mongodb+srv://YogeshBC:Yogesh12345@yogesh.to9jano.mongodb.net/devTinder");
 }
 
 module.exports = connectDB;
